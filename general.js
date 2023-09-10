74abf989a36d0341ec8ee0a2c9c780e1ddf88a55
David Lara Arango <david.lara.arango@atea.no>
2023-09-10 16:06:36 +0200

    Adding files

---
+++ general.js
@@ -0,0 +1,3 @@
+// displays a copyright with the current year and BIC
+document.getElementById("foot1").innerHTML =
+"<p>&copy;  " + new Date().getFullYear() + " Bergen International, Lien 45, 5057, Bergen.</p><p> The sign up sheets are on Google, and may be accessed by anyone with the link. Be careful therefore not to include any information you would not want to have online. </p>";
